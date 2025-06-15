aae77d7a2a58cc3eca130c3a297d01fa58a6d718
Perustream <micasaperu21@gmail.com>
2025-06-15 13:13:41 -0500

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,2 @@
+Este paquete incluye el fondo personalizado y el ícono PWA definidos por el usuario.
+Asegúrate de tener conexión a internet para que carguen desde los enlaces postimg.cc
---
+++ service-worker.js
@@ -0,0 +1,12 @@
+
+self.addEventListener('install', e => {
+  console.log('Service Worker instalado');
+  e.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', e => {
+  console.log('Service Worker activado');
+  return self.clients.claim();
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
